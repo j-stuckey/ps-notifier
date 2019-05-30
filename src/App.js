4f107c6d0a9a8133329d98526eb343acccea91cf
James Stuckey <jstuckey@rutgers.edu>
2019-05-29 20:03:58 -0400

    Fetching an example gist from my own gists to test
    My next step is to formulate the dates properly after
    gathering the data for revision times. I'll need that in order
    to display how long ago the changes were put into effect. I'll
    use moment.js for displaying the dates in a proper format. I need
    to figure out the logic for getting all the data needed (content
    and title, first).

--- src/App.js
+++ src/App.js
@@ -1,22 +1,56 @@
 import React from 'react';
 import './App.css';
+import axios from 'axios';
+
+import { GITHUB_BASE_URL } from './config';
 
 class App extends React.Component {
     constructor(props) {
         super(props);
 
-        this.state = {};
-	}
+        this.state = { gistData: null, files: [] };
+    }
+
+    componentDidMount() {
+        const gistId = 'c0e89effa749de86473b43845918b130';
+        console.log('Mounting');
+
+        // This uses v3 of the Github API
+        // grabs gist from github for changelog
+        axios.get(`${GITHUB_BASE_URL}/gists/${gistId}`).then(res => {
+            this.setState({ gistData: res.data });
+        });
+    }
+
+    componentDidUpdate() {
+        const { gistData } = this.state;
+        console.log('Updating...');
+		console.log(gistData);
+		
+		const history = gistData.history.map(item => {
+			return new Date(item.committed_at);
+		});
+		console.log(history);
+
+        // fetch each of the files and content
+        const files = Object.values(gistData.files);
+        console.log(files);
+    }
 
     render() {
+        const { data } = this.state;
         return (
             <div className="App">
-                <header></header>
+                <header>Hello world</header>
 
-				<h1>Hello,</h1>
+                <ChangelogFile data={null} />
             </div>
         );
     }
 }
+
+const ChangelogFile = props => {
+    return <h1>{props.description || 'default'}</h1>;
+};
 
 export default App;
