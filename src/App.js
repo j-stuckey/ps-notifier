88b5c3785862d616ff9a2d38a84577d6a10a5bae
James Stuckey <jstuckey@rutgers.edu>
2019-05-29 23:29:42 -0400

    Making fetch call using async await

--- src/App.js
+++ src/App.js
@@ -14,31 +14,28 @@ class App extends React.Component {
         };
     }
 
-    componentDidMount() {
+    async componentDidMount() {
         const gistId = 'c0e89effa749de86473b43845918b130';
-        console.log('Mounting');
 
         // This uses v3 of the Github API
-        // grabs gist from github for changelog
-        axios.get(`${GITHUB_BASE_URL}/gists/${gistId}`).then(res => {
-            this.setState({
-                gistData: res.data,
-                files: Object.values(res.data.files)
-            });
-        });
+		// grabs gist from github for changelog
+		const response = await axios.get(`${GITHUB_BASE_URL}/gists/${gistId}`);
+
+		this.setState({ 
+			gistData: response.data,
+			files: Object.values(response.data.files)
+		});
     }
 
     componentDidUpdate() {
         const { gistData } = this.state;
-        console.log('Updating...');
-        console.log(gistData);
+        
 
         // Here I am trying to get the info that
         // will display how long ago the changes were
         const history = gistData.history.map(item => {
             return new Date(item.committed_at);
         });
-        // console.log(history);
     }
 
     render() {
